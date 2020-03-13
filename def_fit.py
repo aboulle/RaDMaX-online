0f5600009c8f165e540f4a9ed5914656e7640220
aboulle <dr.boulle@gmail.com>
2020-03-13 08:45:59 +0100

    Test alternate fitting routines; not conclusive yet

--- def_fit.py
+++ def_fit.py
@@ -1,6 +1,6 @@
 # coding=utf8
 import numpy as np
-from scipy.optimize import least_squares
+from scipy.optimize import least_squares, dual_annealing
 from def_XRD import *
 
 def fit_curve(x, y, param, cst):
@@ -16,6 +16,11 @@ def fit_curve(x, y, param, cst):
 		y_cal = f_Refl(x, param, cst)[0]
 		return (np.log10(y) - np.log10(y_cal))
 
+	if cst["algo"] == 'Test':
+		res_lsq = dual_annealing(residual, x0=param, bounds=np.c_[bounds_min, bounds_max], restart_temp_ratio=0.0, args=(x, y,cst))
+		param_fit = res_lsq.x
+		return param_fit[0:Nw], param_fit[Nw: 2*Nw]
+	
 	if cst["algo"] == 'Least squares':
 		res_lsq = least_squares(residual, param, bounds=(bounds_min, bounds_max), args=(x, y,cst))
 		param_fit = res_lsq.x
@@ -26,6 +31,29 @@ def fit_curve(x, y, param, cst):
 		param_fit = res_lsq.x
 		return param_fit[0:Nw], param_fit[Nw: 2*Nw]
 
+def SMB_fit(residual, x, y, cst, p0, bounds=(-np.inf, np.inf), max_nfev=2, mbmult = 1, epochs = 100):
+	
+	for epoch in range(epochs):
+		print(epoch)
+		m = len(x)
+		minibatch_size = mbmult * len(p0)
+		
+		shuffled_indices = np.random.permutation(m)
+		x_shuffled = x[shuffled_indices]
+		y_shuffled = y[shuffled_indices]
+		
+		for i in range(0, m, minibatch_size):
+			xi = x[i:i+minibatch_size]
+			yi = y[i:i+minibatch_size]
+			
+			cst["resol"] = f_resol(xi, p0, "Pseudo-Voigt")
+			
+			mb_lsq = least_squares(residual, p0, bounds=bounds, max_nfev=max_nfev, args=(xi, yi, cst))
+			p0 = mb_lsq.x
+	
+	return p0
+
+	
 
 	#t0 = time()
 
